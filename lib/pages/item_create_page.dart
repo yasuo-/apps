33b785a09ee0f31d8b97d02e8006fb6f6d93b2d7
yasuo- <kurihara@dripbird.jp>
2024-10-29 00:34:24 +0900

    feat: Add Create page

---
+++ lib/pages/item_create_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ItemCteatePage extends StatelessWidget {
+  const ItemCteatePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Item Create'),
+      ),
+      body: const Center(
+        child: Text('Item Create'),
+      ),
+    );
+  }
+}
