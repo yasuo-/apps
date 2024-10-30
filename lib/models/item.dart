edf1ac0f767b2071aaf679da39f3ff9d775b0486
yasuo- <kurihara@dripbird.jp>
2024-10-30 13:24:53 +0900

    feat: modelとrepository追加

---
+++ lib/models/item.dart
@@ -0,0 +1,41 @@
+/// Itemモデルクラス
+class Item {
+  Item({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.location,
+    required this.count,
+    required this.createdAt,
+  });
+
+  // serverから受け取った時にDartに変える
+  factory Item.fromJson(Map<String, dynamic> json) {
+    return Item(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      description: json['description'] as String,
+      location: json['location'] as String,
+      count: json['count'] as int,
+      createdAt: DateTime.parse(json['created_at'] as String),
+    );
+  }
+
+  // 型を指定している
+  final String id;
+  final String name;
+  final String description;
+  final String location;
+  final int count;
+  final DateTime createdAt;
+
+  // serverに送る時jsonに変える
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'description': description,
+      'location': location,
+      'count': count,
+    };
+  }
+}
---
+++ lib/repositories/item_repository.dart
@@ -0,0 +1,77 @@
+import 'package:apps/models/item.dart';
+import 'package:dio/dio.dart';
+
+/// IItemRepositoryクラス
+abstract class IItemRepository {
+  Future<List<Item>> fetchItems();
+  Future<Item> fetchItem(String id);
+  Future<void> createItem(Item item);
+  Future<void> updateItem(Item item);
+  Future<void> deleteItem(String id);
+}
+
+/// ItemRepositoryクラス
+class ItemRepository implements IItemRepository {
+  ItemRepository(this._dio);
+  final Dio _dio;
+
+  /// 備品一覧を取得する
+  @override
+  Future<List<Item>> fetchItems() async {
+    try {
+      final response = await _dio.get<List<dynamic>>('/items');
+      // 確認用に追加
+      print('--fetchItems:response: $response');
+      final data = response.data!;
+      return data
+          .map((json) => Item.fromJson(json as Map<String, dynamic>))
+          .toList();
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  /// 備品を取得する
+  @override
+  Future<Item> fetchItem(String id) async {
+    try {
+      final response = await _dio.get<dynamic>('/items/$id');
+      final data = response.data;
+      return Item.fromJson(data as Map<String, dynamic>);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  /// 備品を作成する
+  @override
+  Future<void> createItem(Item item) async {
+    try {
+      final body = item.toJson();
+      await _dio.post<void>('/items', data: body);
+    } catch (e) {
+      print('Error in createItem: $e');
+      rethrow;
+    }
+  }
+
+  /// 備品を更新する
+  @override
+  Future<void> updateItem(Item item) async {
+    try {
+      await _dio.put<void>('/items/${item.id}', data: item.toJson());
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  /// 備品を削除する
+  @override
+  Future<void> deleteItem(String id) async {
+    try {
+      await _dio.delete<void>('/items/$id');
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
